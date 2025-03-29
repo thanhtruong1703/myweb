3ac1aba5c409d25bf25c6e023c6c4d3510d77f12
thanhtruong1703 <thanhvnpt1221la@gmail.com>
2025-03-29 23:17:39 +0700

    ohno

--- js_c/acc.js
+++
@@ -1,6 +0,0 @@
-// Lấy tên từ localStorage
-//const username= localStorage.getItem('userButton');
-
-let userlocal = JSON.parse(localStorage.getItem("userlogin")) || [];
-//console.log(userlocal.name).userlocal;
-document.getElementById('userButton').textContent = userlocal.name;
